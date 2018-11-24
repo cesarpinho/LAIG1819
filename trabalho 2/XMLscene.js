80c370f033ff0fede116906611092fa4c6969f09
Unknown <migud.carvalho@gmail.com>
2018-11-24 19:29:41 +0000

    special camara (press C)

--- trabalho 2/XMLscene.js
+++ trabalho 2/XMLscene.js
@@ -112,31 +112,74 @@ class XMLscene extends CGFscene {
         this.interface.addViewsGroup(this.graph.views);
 
         this.sceneInited = true;
+
+        this.initKeys();
     }
 
     /**
      * Keys handler called on display function
      */
-    checkKeys()	{
-  		var text = "Keys pressed: ";
-  		var keysPressed = false;
+    checkKeys()
+	{
 
-  		if (this.gui.isKeyPressed("KeyM")){
-              this.graph.incMaterialsN();
-  			text += " M ";
-  			keysPressed=true;
-  		}
+        //      reset keys
 
-  		if (this.gui.isKeyPressed("KeyO")){
-              this.graph.tvon = !this.graph.tvon;
-  			text += " O ";
-  			keysPressed=true;
-  		}
+        for(var i = 0 ; i < this.keysPressed.length ; i++){
+            this.keysPressed[i] = 0;
+        }
 
-  		if (keysPressed)
-  		    console.log(text);
+		var text="Keys pressed: ";
+		var keysPressed=false;
 
-    }
+		if (this.gui.isKeyPressed("KeyM")){
+            this.keysPressed[0] = 1
+            this.graph.incMaterialsN();
+			text+=" M ";
+			keysPressed=true;
+		}
+
+		if (this.gui.isKeyPressed("KeyO")){
+            this.keysPressed[1] = 1
+            this.graph.tvon = !this.graph.tvon;
+			text+=" O ";
+			keysPressed=true;
+		}
+
+		if (this.gui.isKeyPressed("KeyA")){
+            this.keysPressed[2] = 1
+			text+=" A ";
+			keysPressed=true;
+		}
+
+		if (this.gui.isKeyPressed("KeyD")){
+            this.keysPressed[3] = 1
+			text+=" D ";
+			keysPressed=true;
+		}
+
+		if (this.gui.isKeyPressed("KeyW")){
+            this.keysPressed[4] = 1
+			text+=" W ";
+			keysPressed=true;
+		}
+
+		if (this.gui.isKeyPressed("KeyS")){
+            this.keysPressed[5] = 1
+			text+=" S ";
+			keysPressed=true;
+        }
+
+        if (this.gui.isKeyPressed("KeyC")){
+            console.log("d");
+            this.keysPressed[6] = 1
+            this.movecamera = !this.movecamera;
+			text+=" C ";
+			keysPressed=true;
+		}
+
+		if (keysPressed)
+		console.log(text);
+	}
 
     update(currTime)
 	{
@@ -153,6 +196,9 @@ class XMLscene extends CGFscene {
 		// Verify the keys pressed
 		this.checkKeys();
 
+    if(this.sceneInited)
+    if(this.movecamera)
+      this.updateCamera();
 	};
 
   /**
@@ -220,5 +266,83 @@ class XMLscene extends CGFscene {
 
         this.popMatrix();
         // ---- END Background, camera and axis setup
+    }
+
+    initKeys(){
+        this.keysPressed = [];
+        this.keysPressed[0] = 0;       // M
+        this.keysPressed[1] = 0;       // O
+        this.keysPressed[2] = 0;       // A
+        this.keysPressed[3] = 0;       // D
+        this.keysPressed[4] = 0;       // W
+        this.keysPressed[5] = 0;       // S
+        this.keysPressed[6] = 0;       // C
+
+    }
+
+    updateCamera(){
+
+        if(this.keysPressed[4])
+        this.cameraTranslate(0);
+        if(this.keysPressed[5])
+        this.cameraTranslate(1);
+        if(this.keysPressed[3])
+        this.cameraRotate(0);
+        if(this.keysPressed[2])
+        this.cameraRotate(1);
+        /*
+        this.camera.rotate(vec3.fromValues(1,0,0),-0.01);
+        if(this.keysPressed[5])
+        this.camera.rotate(vec3.fromValues(1,0,0),0.01);
+        */
+
+
+    }
+
+    cameraTranslate(t){
+        switch(t){
+            case 0: //  translate FRONT
+                this.camera.zoom(1);
+                this.movetarget(1);
+                break;
+            case 1: //  translate BACK
+                this.camera.zoom(-1);
+                this.movetarget(-1);
+                break;
+            default:
+            break;
+        }
+    }
+
+    cameraRotate(r){
+        switch(r){
+            case 0: //  rotate RIGHT
+            this.angle+=0.05;
+            this.camera.setTarget(vec3.fromValues(this.distance*Math.cos(this.angle + 225*Math.PI/180) + this.camera.position[0], this.camera.target[1], this.distance*Math.sin(this.angle + 225*Math.PI/180) + this.camera.position[2]));
+                break;
+            case 1: //  rotate LEFT
+            this.angle-=0.05;
+            this.camera.setTarget(vec3.fromValues(this.distance*Math.cos(this.angle + 225*Math.PI/180) + this.camera.position[0], this.camera.target[1], this.distance*Math.sin(this.angle + 225*Math.PI/180) + this.camera.position[2]));
+                break;
+            default:
+            break;
+        }
+    }
+
+    movetarget(d){
+        var dir = this.camera.calculateDirection();
+        console.log(this.camera.target[0]);
+        console.log(dir[0]);
+        console.log(dir[1]);
+        console.log(dir[2]);
+        console.log("x: " + this.camera.target[0]);
+        console.log("y: " + this.camera.target[1]);
+        console.log("z: " + this.camera.target[2]);
+        this.camera.setTarget(vec3.fromValues(this.camera.target[0] + dir[0] * d, this.camera.target[1] + dir[1] * d, this.camera.target[2] + dir[2] * d));
+        console.log(dir);
+    }
+
+    distance2points(a,b){
+        return Math.sqrt(Math.pow((a[0]-b[0]),2) + Math.pow((a[1]-b[1]),2) + Math.pow((a[2]-b[2]),2));
     }
 }
